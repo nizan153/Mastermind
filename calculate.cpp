c5b5d5ffa388dcd30533b83bf26c7874db736d9e
nizan153 <nizan153@gmail.com>
2019-05-06 07:08:25 +0300

    fix

--- calculate.cpp
+++ calculate.cpp
@@ -8,19 +8,20 @@ string calculateBullAndPgia(const string chosen, const string guess) {
     if(chosen.length() != guess.length()) {
         throw "Guess too long/short";
     }
-    uint bull = 0;
-    uint pgia = 0;
+    int bull = 0;
+    int pgia = 0;
 
-    uint count[10] = { 0 };
+    int count[10] = { 0 };
 
-    for(uint i = 0; i < chosen.length(); i++) {
+    for(int i = 0; i < chosen.length(); i++) {
         if(guess.at(i) == chosen.at(i)) { bull++; }
         else {
-            if(count[chosen.at(i)-'0']++ != 0) { pgia++; }
-            if(count[guess.at(i)-'0']-- != 0) { pgia++; }
+            if(count[guess.at(i)-'0']++ < 0) { pgia++; }
+            if(count[chosen.at(i)-'0']-- > 0) { pgia++; }
             
         }
     }
-    return std::to_string(bull) + "," + std::to_string(pgia);
+    std::string str = std::to_string(bull) + "," + std::to_string(pgia);
+    return str;
 
 }
